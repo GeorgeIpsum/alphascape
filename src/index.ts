0059d5d5c86e0ccef832d626faf4e601bd15976c
Ibrahim <georgeipsum@gmail.com>
2021-07-19 18:07:18 -0400

    look. im lonely. ok.

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,34 @@ client.on('ready', () => {
   console.log('ready');
 });
 
+client.on('message', m => {
+  if((process.env.SON && m.content.indexOf(process.env.SON) > 0) || m.content.indexOf('!a4') === 0) {
+    m.channel.send('Hello, world!');
+  }
+
+  if(m.author.id === process.env.FATHER) {
+    m.channel.send("Hi dad!!!!!!");
+  }
+});
+
+client.on('messageReactionAdd', m => {
+
+});
+
+client.on('messageReactionRemove', m => {
+
+});
+
+client.on('messageReactionRemoveAll', m => {
+
+});
+
+client.on('messageReactionRemoveEmoji', m => {
+
+});
+
+client.on('webhookUpdate', update => {
+
+});
+
 client.login(process.env.TOKEN);
